c2918ac494d427750c28c9a15e619c1e049e4017
Bashir <b_egeh@hotmail.com>
2020-04-28 21:16:04 -0400

    api key is sent from config.js, to the server, and then used to render the script tag on the DOM

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const config = require("../public/javascripts/config");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GYM FINDER' });
+  res.render('index', { title: 'GYM FINDER', key: config.MY_KEY});
 });
 
 module.exports = router;
