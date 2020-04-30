61fcaffc9d39a1856a01a00b6ce2e2eacf19e77a
Bashir <b_egeh@hotmail.com>
2020-04-30 17:36:08 -0400

    add title infowindows to markers generated as search result

--- public/javascripts/getAxios.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-
-function getAxios(){
-  return axios.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+toronto+canada&key=AIzaSyB5Gt5W_NoLCy1k3_pbqPiDwjcrobZ5G7Q")
-  .then(res => console.log(res.data));
-}
