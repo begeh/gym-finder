517cae56086f43e4ee88762610a350e0b36bd85b
Bashir <b_egeh@hotmail.com>
2020-04-30 14:47:46 -0400

    add search bar to map

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const config = require("../public/javascripts/config");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   res.render('index', { title: 'GYM FINDER', key: config.MY_KEY});
 });
 
